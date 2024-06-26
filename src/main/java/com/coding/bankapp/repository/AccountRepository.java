6b3689cd889464340a6fc8c6a3cf13d82dc6d554
mnasnh <mnasnh@gmail.com>
2024-06-26 23:52:16 +0100

    Add concurrency control for updating account balance

--- src/main/java/com/coding/bankapp/repository/AccountRepository.java
+++ src/main/java/com/coding/bankapp/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.coding.bankapp.repository;
 
 import com.coding.bankapp.repository.entity.AccountEntity;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,14 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends CrudRepository<AccountEntity, Long> {
+    /**
+     * Find by account number and account status
+     * Hold PESSIMISTIC_WRITE lock to prevent other transactions from
+     * reading and updating the account balance.
+     * @param accountNumber
+     * @param status
+     * @return
+     */
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<AccountEntity> findByAccountNumberAndAccountStatus(Long accountNumber, String status);
 }
